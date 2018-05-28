bdfd9da70f1c09b8d329ac31c537152050bd4c6d
liyong <yong.li21@pactera.com>
2018-05-28 17:39:21 +0800

    测试分支提交到主分支

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("git frist commit!");
-		System.out.println("1111");
+		System.out.println("testBranch commit");
 		
 		System.out.println("git second commit!");
 		
